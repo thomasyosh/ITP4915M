ef9129f24fd02864277d29911c93224d5ec34ef6
Thomas <itada1@landsd.gov.hk>
2025-05-28 13:33:04 +0800

    nextjs integrated

---
+++ strapi/config/server.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: 'http://strapi/cms',
+  admin: {
+    url: 'http://strapi/dashboard',
+  },
+});
