5900901d65f629cb539d9b889fafa93a3653c9d1
thomasyosh <thomas.yosh@tutanota.com>
2025-06-16 06:04:05 +0000

    some testing on nextjs

---
+++ recipe_generator/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/darkmode-button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>page</div>
+    </>
+    
+  )
+}
+
+export default page
